201968431637b96327eefa393953444591028cb9
Justin <justin@Yudis-MacBook-Pro.local>
2025-04-17 17:17:13 -0400

    new model

--- Part2/test.py
+++ Part2/test.py
@@ -3,21 +3,27 @@ import torch
 from torch import nn
 import torch.optim as optim
 from torch.utils.data import Dataset
-from torchvision import datasets
+from torchvision import datasets, transforms, models
 from torchvision.transforms import ToTensor
 import time
 import sys
 
 ## download dataset ##
 
-training_data = datasets.FashionMNIST(
+transform = transforms.Compose([
+    transforms.Resize(224),  # Required for ResNet input
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+training_data = datasets.CIFAR100(
     root="data",
     train=True,
     download=True,
     transform=ToTensor()
 )
 
-test_data = datasets.FashionMNIST(
+test_data = datasets.CIFAR100(
     root="data",
     train=False,
     download=True,
@@ -58,12 +64,8 @@ input_size = 784
 hidden_sizes = [128, 64]
 output_size = 10
 
-model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
-                      nn.ReLU(),
-                      nn.Linear(hidden_sizes[0], hidden_sizes[1]),
-                      nn.ReLU(),
-                      nn.Linear(hidden_sizes[1], output_size),
-                      nn.LogSoftmax(dim=1))
+model = models.resnet18(pretrained=False)
+model.fc = nn.Linear(model.fc.in_features, 100)
 
 if gpus > 1:
     model = nn.DataParallel(model, device_ids=device_ids)
@@ -73,48 +75,34 @@ model.to(device)
 ## train the model ##
 
 # Define the loss
-criterion = nn.NLLLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.003)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
 epochs = 5
 for e in range(epochs):
     running_loss = 0
+    model.train()
     for images, labels in trainloader:
         images, labels = images.to(device), labels.to(device)
-        # Flatten MNIST images into a 784 long vector
-        images = images.view(images.shape[0], -1)
-    
-        # Training pass
         optimizer.zero_grad()
-        
-        output = model(images)
-        loss = criterion(output, labels)
+        outputs = model(images)
+        loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
-        
         running_loss += loss.item()
-    else:
-        print(f"Training loss: {running_loss/len(trainloader)}")
+    print(f"Training loss: {running_loss/len(trainloader)}")
 
 ## test model ##
 
-correct_count, all_count = 0, 0
-for images,labels in testloader:
-  for i in range(len(labels)):
-    img = images[i].view(1, 784).to(device)
-    # Turn off gradients to speed up this part
-    with torch.no_grad():
-        logps = model(img)
+model.eval()
+correct, total = 0, 0
+with torch.no_grad():
+    for images, labels in testloader:
+        images, labels = images.to(device), labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
 
-    # Output of the network are log-probabilities, 
-    # need to take exponential for probabilities
-    ps = torch.exp(logps)
-    probab = list(ps.cpu().numpy()[0])
-    pred_label = probab.index(max(probab))
-    true_label = labels.numpy()[i]
-    if(true_label == pred_label):
-      correct_count += 1
-    all_count += 1
-
-print("Number Of Images Tested =", all_count)
-print("\nModel Accuracy =", (correct_count/all_count))
+print(f"Number Of Images Tested = {total}")
+print(f"Model Accuracy = {correct / total:.4f}")
